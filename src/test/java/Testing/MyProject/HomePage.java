c16966a3405fdf9968ee34be0a15988b40240fb2
Mouli Sarkar <Mouli Sarkar@192.168.0.4>
2021-05-05 20:03:31 +0530

    c

--- src/test/java/Testing/MyProject/HomePage.java
+++ src/test/java/Testing/MyProject/HomePage.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -44,6 +45,7 @@ public class HomePage extends Base {
 		
 		System.out.println(text);
 		lo.getLogin().click();
+		
 		
 		
 	}
